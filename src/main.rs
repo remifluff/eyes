1e0afff586aeeb09a2f0bda264d516d657a241fe
remifluff <remi.cady.freer@gmail.com>
2022-09-09 20:49:45 +1000

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 use core::num;
+use std::time::Instant;
 use std::{os::unix::prelude::DirEntryExt, string, time::Duration};
 
 use nannou::draw;
+use nannou::image::{DynamicImage, GrayImage};
 use nannou::{draw::background::new, ease, prelude::*, wgpu::ToTextureView};
+use nokhwa::{Camera, CameraFormat, FrameFormat};
 use osc::Message;
+use wgpu::Texture;
 
 pub mod connection;
 use crate::fbo::Fbo;
@@ -20,23 +24,39 @@ pub use serial2::SerialPort;
 const PORT_NAME: &str = "/dev/cu.usbmodem105641701";
 static mut PORT: Connection = Connection::new(PORT_NAME, false);
 
+use nannou::image::DynamicImage::ImageRgb8;
 use nannou_osc as osc;
 
 const OSC_PORT: u16 = 8338;
 
-fn main() {
+extern crate rustface;
+
+use rustface::{Detector, FaceInfo, ImageData};
+
+fn main() -> Result<(), ()> {
     nannou::app(model).update(update).simple_window(view).run();
+    Ok(())
 }
 
 pub struct Model {
     eye: Eye,
     write_timer: Timer,
     screen: [Screen; 2],
+    camera: Camera,
     receiver: osc::Receiver,
     received_packets: Vec<(std::net::SocketAddr, osc::Packet)>,
+    image: Option<DynamicImage>,
 }
 
 fn model(app: &App) -> Model {
+    let mut camera = Camera::new(
+        0,                                                              // index
+        Some(CameraFormat::new_from(640, 480, FrameFormat::MJPEG, 30)), // format
+    )
+    .unwrap();
+    // open stream
+
+    camera.open_stream().unwrap();
     // Bind an `osc::Receiver` to a port.
     // added mroe comments
     let receiver = osc::receiver(OSC_PORT).unwrap();
@@ -64,27 +84,66 @@ fn model(app: &App) -> Model {
         eye,
         write_timer,
         screen,
+        camera,
         receiver,
         received_packets,
+        image: None,
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    for (packet, addr) in model.receiver.try_iter() {
-        model.received_packets.push((addr, packet));
+    let options = match Options::parse(std::env::args()) {
+        Ok(options) => options,
+        Err(message) => {
+            println!("Failed to parse program arguments: {}", message);
+            std::process::exit(1)
+        }
+    };
+
+    let mut detector = match rustface::create_detector(options.model_path()) {
+        Ok(detector) => detector,
+        Err(error) => {
+            println!("Failed to create detector: {}", error.to_string());
+            std::process::exit(1)
+        }
+    };
+
+    detector.set_min_face_size(20);
+    detector.set_score_thresh(2.0);
+    detector.set_pyramid_scale_factor(0.8);
+    detector.set_slide_window_step(4, 4);
+
+    // let image: DynamicImage = match image::open(options.image_path()) {
+    //     Ok(image) => image,
+    //     Err(message) => {
+    //         println!("Failed to read image: {}", message);
+    //         std::process::exit(1)
+    //     }
+    // };
+
+    let mut rgb = model.image.unwrap().to_rgb8();
+    let faces = detect_faces(&mut *detector, &model.image.unwrap().to_luma8());
+
+    for face in faces {
+        let bbox = face.bbox();
+        let rect = Rect::at(bbox.x(), bbox.y()).of_size(bbox.width(), bbox.height());
+
+        draw_hollow_rect_mut(&mut rgb, rect, Rgb([255, 0, 0]));
     }
 
-    // We'll display 10 packets at a time, so remove any excess.
-    let max_packets = 10;
-    while model.received_packets.len() > max_packets {
-        model.received_packets.remove(0);
+    match rgb.save(OUTPUT_FILE) {
+        Ok(_) => println!("Saved result to {}", OUTPUT_FILE),
+        Err(message) => println!("Failed to save result to a file. Reason: {}", message),
     }
+    // println!("{}, {}", frame.width(), frame.height());
 
     let t = app.time;
 
     model.eye.set_center(app.mouse.position());
     model.eye.update_openess(t.blink_ease(1.0));
     if model.write_timer.check(t) {}
+
+    model.image = Some(ImageRgb8(model.camera.frame().unwrap()));
 
     for screen in &model.screen {
         let draw = screen.draw();
@@ -101,57 +160,37 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
 
-    // Create a string showing all the packets.
-    let mut packets_text = format!("Listening on port {}\nReceived packets:\n", OSC_PORT);
+    let texture = Texture::from_image::<&App>(&app, &model.image.as_ref().unwrap());
 
-    for (addr, packet) in model.received_packets.iter().rev() {
-        let x: Option<(i32, i32)> = if let osc::Packet::Message(m) = packet {
-            match m.addr.as_str() {
-                "/pose/position" => {
-                    if let Some(args) = &m.args {
-                        None
-                    } else {
-                        None
-                    }
-                }
-
-                _ => None,
-            }
-        } else {
-            None
-        };
-
-        //if m.addr.as_str() == "/pose/position" {
-
-        // match packet {
-        //     osc::Packet::Message(m) => matr,
-        //     osc::Packet::Bundle(_) => todo!(),
-        // }
-    }
-
-    for &(addr, ref packet) in model.received_packets.iter().rev() {
-        packets_text.push_str(&format!("{}: {:?}\n", addr, packet));
-    }
-
-    print!("{}", packets_text);
-    let rect = frame.rect().pad(10.0);
-    draw.text(&packets_text)
-        .font_size(16)
-        .align_text_top()
-        .line_spacing(10.0)
-        .left_justify()
-        .wh(rect.wh());
+    // let rect = frame.rect().pad(10.0);
 
     let win = app.window_rect();
 
     // draw.texture(&m.screen.fbo.texture).w_h(600.0, 600.0);
     // draw.texture(&m.screen.fbo.texture).w_h(20.0, 20.0);
+
     for screen in &model.screen {
         screen.draw_to_frame(app);
     }
+    draw.texture(&texture).w_h(600.0, 600.0);
 
     draw.to_frame(app, &frame).unwrap();
 }
+
+fn detect_faces(detector: &mut dyn Detector, gray: &GrayImage) -> Vec<FaceInfo> {
+    let (width, height) = gray.dimensions();
+    let mut image = ImageData::new(gray, width, height);
+    let now = Instant::now();
+    let faces = detector.detect(&mut image);
+    println!(
+        "Found {} faces in {} ms",
+        faces.len(),
+        get_millis(now.elapsed())
+    );
+    faces
+}
+
+fn detect() {}
 
 struct Eye {
     x: f32,
@@ -267,5 +306,39 @@ impl EaseExt for f32 {
     fn blink_ease(&self, d: f32) -> f32 {
         let t = *self % (d * 2.0);
         ease::sine::ease_in_out(t, 0.0, 1.0, d)
+    }
+}
+
+fn get_millis(duration: Duration) -> u64 {
+    duration.as_secs() * 1000u64 + u64::from(duration.subsec_nanos() / 1_000_000)
+}
+
+struct Options {
+    image_path: String,
+    model_path: String,
+}
+
+impl Options {
+    fn parse(args: Args) -> Result<Self, String> {
+        let args: Vec<String> = args.into_iter().collect();
+        if args.len() != 3 {
+            return Err(format!("Usage: {} <model-path> <image-path>", args[0]));
+        }
+
+        let model_path = args[1].clone();
+        let image_path = args[2].clone();
+
+        Ok(Options {
+            image_path,
+            model_path,
+        })
+    }
+
+    fn image_path(&self) -> &str {
+        &self.image_path[..]
+    }
+
+    fn model_path(&self) -> &str {
+        &self.model_path[..]
     }
 }
