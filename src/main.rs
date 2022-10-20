9ab59922b04c775c7e01417b7b049fd6186447de
remi-freer <remi@remifreer.com>
2022-10-20 15:06:39 +1100

    seperated out settings

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
 #![allow(dead_code)]
-#![allow(unused_imports)]
-use chrono::{TimeZone, Utc};
-use core::num;
-use nannou::draw;
-use nannou::draw::primitive::rect;
-use nannou::lyon::geom::euclid::SideOffsets2D;
-use nannou::{
-    draw::background::new, ease, prelude::*, wgpu::ToTextureView,
-};
-use nannou_egui::egui::Slider;
-
-use time::macros::{date, datetime};
-
-use osc::Message;
-use rustface::FaceInfo;
-use std::time::Instant;
-use std::time::{Duration, SystemTime};
-use std::{os::unix::prelude::DirEntryExt, string};
-use time::Weekday::Wednesday;
-use time::{Date, OffsetDateTime, PrimitiveDateTime, UtcOffset};
-use wgpu::Texture;
+use nannou::prelude::*;
 
 pub mod connection;
 
@@ -27,12 +7,6 @@ use crate::connection::Connection;
 
 mod scraen;
 use scraen::Scraen;
-
-mod ui;
-use ui::UI;
-
-// mod vision;
-// use vision::Vision;
 
 mod webcam;
 use webcam::Webcam;
@@ -43,43 +17,8 @@ use timer::Timer;
 mod walk;
 use walk::Walk;
 
-pub use serial2::SerialPort;
-
-const PORT_NAME: &str = "/dev/ttyprintk";
-// const PORT_NAME: &str = "/dev/ttyACM0";
-
-use nannou::image::DynamicImage::ImageRgb8;
-use nannou_osc as osc;
-// use nokhwa::{
-//     query, query_devices, Camera, CameraFormat, CaptureAPIBackend,
-//     FrameFormat, Resolution, ThreadedCamera,
-// };
-const SCRAEN_SCALE: f32 = 10.0 * SCALE;
-
-const SCRAENS: [ScraenDim; 4] = [
-    ScraenDim {
-        rez: 4,
-        xy: (466.0, -123.0),
-        wh: (4.0, 4.0),
-    },
-    ScraenDim {
-        rez: 16,
-        xy: (102.0, -212.0),
-        wh: (16.0, 16.0),
-    },
-    ScraenDim {
-        rez: 8,
-        xy: (38.0, 92.0),
-        wh: (8.0, 8.0),
-    },
-    ScraenDim {
-        rez: 12,
-        xy: (453.0, 124.0),
-        wh: (12.0, 12.0),
-    },
-];
-
-const OSC_PORT: u16 = 8338;
+mod settings;
+use settings::*;
 
 pub struct ScraenDim {
     rez: u32,
@@ -93,15 +32,15 @@ pub struct Settings {
     circle_count: usize,
 }
 
+pub use serial2::SerialPort;
+
 fn main() {
     nannou::app(model).update(update).run();
 }
-const SHOWDEBUG: bool = true;
 pub struct Model {
     scraens: Vec<Scraen>,
     // vision: Vision,
     // vision2: Vision,
-    ui: UI,
     port: Connection,
 
     face_cam_rect: Rect,
@@ -111,12 +50,6 @@ pub struct Model {
     walk_y: Walk,
     webcam: Webcam,
 }
-const SCALE: f32 = 2.5;
-
-const CAMERA_WH: (u32, u32) = (320, 240);
-
-const WIDTH: f32 = 240.0 * 2.0;
-const HEIGHT: f32 = 360.0 * 1.0;
 
 fn model(app: &App) -> Model {
     let window_id = app
@@ -165,7 +98,6 @@ fn model(app: &App) -> Model {
     Model {
         scraens: screen,
         // vision,
-        ui: UI::new(&window),
         port,
         face_cam_rect,
         street_cam_rect,
